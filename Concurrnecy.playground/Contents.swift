5f78c19115f50320c93a479e684779072905f55c
Mohd Kashif <kashif@Mohds-MacBook-Air.local>
2024-04-08 01:00:21 +0530

    weak and unowedn

--- Concurrnecy.playground/Contents.swift
+++ Concurrnecy.playground/Contents.swift
@@ -25,24 +25,91 @@ PlaygroundPage.current.needsIndefiniteExecution=true
 /// Concurrent multiple task at a time
 ///
 
-var count=0
-DispatchQueue.main.async {  // main queue is serial Queu
-    for i in 0...3{
-        count=i
-        print(count)
-    }
-}
-for i in 4...6{
-    count=i
-    print(count)
-}
-
-DispatchQueue.main.async {
-        count=9
-        print(count)
-}
+//var count=0
+//DispatchQueue.main.async {  // main queue is serial Queu
+//    for i in 0...3{
+//        count=i
+//        print(count)
+//    }
+//}
+//for i in 4...6{
+//    count=i
+//    print(count)
+//}
+//
+//DispatchQueue.main.async {
+//        count=9
+//        print(count)
+//}
 
 //Output would be 4,5,6,0,1,2,3,9
 /// Program start from line 29 we submit our task to main queue which is a serial queue in asynchronous manner meaning only one task at a time without blocking current execution, then
 /// the normal loop executed normally giving the output 4,5,6 then another task submiited to main queue so total task in queue is 2, queue is a fifo order our first task dispatch first whcih
 /// print 0,1,2,3 then second task dispatch whcih is 9
+
+//------------------------------------------------------Part2-------------------------------------------------------------------//
+
+// GCD handled the Dispatch Queue
+/// thre are three types of Dispatch Queues
+/// 1:- main queue
+/// 2:- global queue
+/// 3:- custom queue
+
+// Main Queue:- It is system created, serial queue and uses the main thread
+///
+
+
+
+/// weak and unowned
+/// :- Weak Keyword
+/// use a weak  reference when the other instance have shorter life time- that is when the other instance can be deallocated first.
+///   or simple terms in order to use weak when we find that in our object relationship there is probability that one of the object can be nil
+///  let take the below example a Person either have Bank account or not so it better to use weak while assigning a BankAccount object in Person Class
+///  we shouldnt use let with weak because in future person can open a Bank Account so we can assign a Bank account to user.
+///   we should declare weak property as Optional
+// :- Unowned
+/// Let take this below example of Persona and Bank Account
+/// We should use unowned when a property deos not exist without other other property. A bank account cannot exist without a person so we should declare it should link with a person
+/// so a person property in Bank Account class cannot be optional
+class Person{
+    var name:String
+    var bankAccount:BankAccount?
+    init(_name: String) {
+        name = _name
+        print("person initialized")
+    }
+    
+//    func printName(){
+//        print("name is ", name)
+//    }
+    deinit{
+        print("person deinitilezed")
+    }
+}
+
+class BankAccount{
+    var accNumber:Int
+    unowned var person:Person
+    init(_accNumber: Int, _person: Person) {
+        accNumber = _accNumber
+        person = _person
+        print("Bank initialized")
+    }
+    deinit{
+        print("Bank deinitialized")
+    }
+}
+
+if (1==1){
+    var person:Person?=Person(_name: "kashif")
+    var bank:BankAccount?=BankAccount(_accNumber: 345, _person: person!)
+    person?.bankAccount=bank
+//    bank?.person=person
+//    person?.bankAccount=nil
+//    person=nil
+//    bank=nil
+//    let obj=Person(name: "kashif")
+//    obj.printName()
+}
+
+
